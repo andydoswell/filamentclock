5a67820104f2acbb6447771b930fb7b5856083a8
Andy Doswell <andydoswell@hotmail.com>
2024-08-17 12:47:51 +0100

    Add files via upload
    Fixed the bug which stopped it working on the new IDE (2.3.2) and stopped it after it got to 1000 subs. Should be good to 9,999 now!

--- YouTube_Counter_for_git.ino
+++ YouTube_Counter_for_git.ino
@@ -9,26 +9,26 @@
 */
 #include <YoutubeApi.h>
 #include "EEPROM.h"
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
 #include <WiFiClientSecure.h>
 int hundreds;
 int thousands;
 int extractMin;
 int units;
 int tens;
-char *  ssid;
-char *  pass;
-int testdelay = 1;
-#define API_KEY "insert your youtube API key here"
-#define CHANNEL_ID "insert your youtube channel ID here"
+char* ssid;
+char* pass;
+int testdelay = 100;
+#define API_KEY "Insert your API key here"
+#define CHANNEL_ID "Insert your channel ID here"
 
-#define RST_PIN 32 //reset wifi credentials pin
-#define MODE_PIN 33 // high for 24hours, low for 12.
-#define anode_HH 23 // define 10's of hours anode pin
-#define anode_H 22 // define units of hours anode pin
-#define anode_MM 21 // define 10's of minutes pin
-#define anode_M 19 //define units of minutes pin
-#define cathode_A 18 // define cathode outputs
+#define RST_PIN 32    //reset wifi credentials pin
+#define MODE_PIN 33   // high for 24hours, low for 12.
+#define anode_HH 23   // define 10's of hours anode pin
+#define anode_H 22    // define units of hours anode pin
+#define anode_MM 21   // define 10's of minutes pin
+#define anode_M 19    //define units of minutes pin
+#define cathode_A 18  // define cathode outputs
 #define cathode_B 5
 #define cathode_C 17
 #define cathode_D 16
@@ -38,7 +38,7 @@ int testdelay = 1;
 #define dots 25
 WiFiClientSecure client;
 YoutubeApi api(API_KEY, client);
-unsigned long api_mtbs = 300000; // update ever 5 mins
+unsigned long api_mtbs = 300000;  // delay between api calls
 unsigned long api_lasttime;
 long subs = 0;
 String subs_count;
@@ -46,9 +46,9 @@ String view_count;
 boolean initFlag = true;
 
 void setup() {
-  pinMode(RST_PIN, INPUT_PULLUP); //pull this low for resetting WiFi credentials
-  pinMode(MODE_PIN, INPUT_PULLUP); // pull this low for 12 hrs, open for 24h
-  pinMode(anode_HH, OUTPUT); // set up the display pins
+  pinMode(RST_PIN, INPUT_PULLUP);   //pull this low for resetting WiFi credentials
+  pinMode(MODE_PIN, INPUT_PULLUP);  // pull this low for 12 hrs, open for 24h
+  pinMode(anode_HH, OUTPUT);        // set up the display pins
   pinMode(anode_H, OUTPUT);
   pinMode(anode_MM, OUTPUT);
   pinMode(anode_M, OUTPUT);
@@ -60,13 +60,13 @@ void setup() {
   pinMode(cathode_F, OUTPUT);
   pinMode(cathode_G, OUTPUT);
   pinMode(dots, OUTPUT);
-  digitalWrite (dots, LOW);
+  digitalWrite(dots, LOW);
   // switch all the anodes off
-  digitalWrite (anode_HH, HIGH);
-  digitalWrite (anode_H, HIGH);
-  digitalWrite (anode_MM, HIGH);
-  digitalWrite (anode_M, HIGH);
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  digitalWrite(anode_HH, HIGH);
+  digitalWrite(anode_H, HIGH);
+  digitalWrite(anode_MM, HIGH);
+  digitalWrite(anode_M, HIGH);
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
   Serial.begin(115200);
   //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wm;
@@ -77,16 +77,14 @@ void setup() {
   // then goes into a blocking loop awaiting configuration and will return success result
   bool res;
   // res = wm.autoConnect(); // auto generated AP name from chipid
-  res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("YouTube counter");  // anonymous ap
   //res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
   if (!res) {
     Serial.println("Failed to connect");
-  }
-  else {
+  } else {
     //if you get here you have connected to the WiFi
     Serial.println("connected... :)");
   }
-  //testcathodes();
 }
 
 void loop() {
@@ -95,29 +93,31 @@ void loop() {
     Serial.println("Attempt to reset");
     WiFiManager wm;
     wm.resetSettings();
-    Serial.println ("Wifi credentails reset.");
+    Serial.println("Wifi credentails reset.");
     ESP.restart();
   }
+
+  //testcathodes();
 
   if (millis() - api_lasttime > api_mtbs || initFlag) {
     initFlag = false;
     // all anodes off, and the dots on whilst the api runs
-    digitalWrite (anode_HH, HIGH);
-    digitalWrite (anode_H, HIGH);
-    digitalWrite (anode_MM, HIGH);
-    digitalWrite (anode_M, HIGH);
-    digitalWrite (dots, HIGH);
-    if (api.getChannelStatistics(CHANNEL_ID))
-    {
-      subs_count = api.channelStats.subscriberCount ;
-      view_count = api.channelStats.viewCount ;
+    digitalWrite(anode_HH, HIGH);
+    digitalWrite(anode_H, HIGH);
+    digitalWrite(anode_MM, HIGH);
+    digitalWrite(anode_M, HIGH);
+    digitalWrite(dots, HIGH);
+    Serial.println("Attempting to run API");
+    if (api.getChannelStatistics(CHANNEL_ID)) {
+      subs_count = api.channelStats.subscriberCount;
+      view_count = api.channelStats.viewCount;
+      Serial.println("Got API data");
       int subs_countInt;
-      subs_countInt =  subs_count.toInt();
-      thousands = ((subs_countInt / 1000) % 1000);
-      hundreds = ((subs_countInt / 100) % 100);
+      subs_countInt = subs_count.toInt();
+      thousands = ((subs_countInt / 1000) % 10);
+      hundreds = ((subs_countInt / 100) % 10);
       tens = ((subs_countInt / 10) % 10);
-      units = ((subs_countInt ) % 10);
-
+      units = ((subs_countInt) % 10);
     }
     digitalWrite(dots, LOW);
     api_lasttime = millis();
@@ -125,175 +125,181 @@ void loop() {
   updateFilamentDisplay();
 }
 
-void updateFilamentDisplay () {
-
-  for (int anode = 0; anode <= 3; anode++ ) {
+void updateFilamentDisplay() {
+  //Serial.println("Updating display");
+  for (int anode = 0; anode <= 3; anode++) {
     switch (anode) {
       case 0:
-        digitalWrite (anode_M, HIGH); // switch the last anode off, so all anodes are off.
-
-        setCathode(thousands); // set the cathodes
-
-        digitalWrite (anode_HH, LOW); // switch the anode on
+        digitalWrite(anode_M, HIGH);  // switch the last anode off, so all anodes are off.
+        setCathode(thousands);        // set the cathodes
+        digitalWrite(anode_HH, LOW);  // switch the anode on
         break;
       case 1:
-        digitalWrite (anode_HH, HIGH); // repeat above, cycling through the anodes
+        digitalWrite(anode_HH, HIGH);  // repeat above, cycling through the anodes
         setCathode(hundreds);
-        digitalWrite (anode_H, LOW);
+        digitalWrite(anode_H, LOW);
         break;
       case 2:
-        digitalWrite (anode_H, HIGH);
+        digitalWrite(anode_H, HIGH);
         setCathode(tens);
-        digitalWrite (anode_MM, LOW);
+        digitalWrite(anode_MM, LOW);
         break;
       case 3:
-        digitalWrite (anode_MM, HIGH);
+        digitalWrite(anode_MM, HIGH);
         setCathode(units);
-        digitalWrite (anode_M, LOW);
+        digitalWrite(anode_M, LOW);
+        break;
+
+      default:
+        Serial.print("Unknown anode:");
+        Serial.println(anode);
         break;
     }
-    delay (testdelay);
+    delayMicroseconds(testdelay);
   }
 }
 
-int setCathode (int K) {
+void setCathode(int K) {
+  //Serial.print("Set cathode to ");
   switch (K) {
     case 0:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, HIGH);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, LOW);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, HIGH);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, LOW);
       break;
     case 1:
-      digitalWrite (cathode_A, LOW);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, LOW);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, LOW);
-      digitalWrite (cathode_G, LOW);
+      digitalWrite(cathode_A, LOW);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, LOW);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, LOW);
+      digitalWrite(cathode_G, LOW);
       break;
     case 2:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, LOW);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, HIGH);
-      digitalWrite (cathode_F, LOW);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, LOW);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, HIGH);
+      digitalWrite(cathode_F, LOW);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 3:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, LOW);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, LOW);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 4:
-      digitalWrite (cathode_A, LOW);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, LOW);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, LOW);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, LOW);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 5:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, LOW);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, LOW);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 6:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, LOW);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, HIGH);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, LOW);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, HIGH);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 7:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, LOW);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, LOW);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, LOW);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, LOW);
       break;
     case 8:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, HIGH);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, HIGH);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, HIGH);
       break;
     case 9:
-      digitalWrite (cathode_A, HIGH);
-      digitalWrite (cathode_B, HIGH);
-      digitalWrite (cathode_C, HIGH);
-      digitalWrite (cathode_D, HIGH);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, HIGH);
-      digitalWrite (cathode_G, HIGH);
+      digitalWrite(cathode_A, HIGH);
+      digitalWrite(cathode_B, HIGH);
+      digitalWrite(cathode_C, HIGH);
+      digitalWrite(cathode_D, HIGH);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, HIGH);
+      digitalWrite(cathode_G, HIGH);
       break;
-    case 10: // This is a special case, used to suppress the tens of hours display, if it's 0.
-      digitalWrite (cathode_A, LOW);
-      digitalWrite (cathode_B, LOW);
-      digitalWrite (cathode_C, LOW);
-      digitalWrite (cathode_D, LOW);
-      digitalWrite (cathode_E, LOW);
-      digitalWrite (cathode_F, LOW);
-      digitalWrite (cathode_G, LOW);
+    case 10:  // This is a special case, used to suppress the tens of hours display, if it's 0.
+      digitalWrite(cathode_A, LOW);
+      digitalWrite(cathode_B, LOW);
+      digitalWrite(cathode_C, LOW);
+      digitalWrite(cathode_D, LOW);
+      digitalWrite(cathode_E, LOW);
+      digitalWrite(cathode_F, LOW);
+      digitalWrite(cathode_G, LOW);
+      break;
+    default:
+      Serial.print("Unknown Cathode:");
+      Serial.println(K);
       break;
   }
 }
 
-void testcathodes () {
+void testcathodes() {
   int td = 1500;
-  digitalWrite (anode_HH, LOW);
-  digitalWrite (anode_H, LOW);
-  digitalWrite (anode_MM, LOW);
-  digitalWrite (anode_M, LOW);
-  digitalWrite (cathode_A, HIGH);
-  Serial.println ("A");
-  delay (td);
-  digitalWrite (cathode_A, LOW);
-  digitalWrite (cathode_B, HIGH);
-  Serial.println ("B");
-  delay (td);
-  digitalWrite (cathode_B, LOW);
-  digitalWrite (cathode_C, HIGH);
-  Serial.println ("C");
-  delay (td);
-  digitalWrite (cathode_C, LOW);
-  digitalWrite (cathode_D, HIGH);
-  Serial.println ("D");
-  delay (td);
-  digitalWrite (cathode_D, LOW);
-  digitalWrite (cathode_E, HIGH);
-  Serial.println ("E");
-  delay (td);
-  digitalWrite (cathode_E, LOW);
-  digitalWrite (cathode_F, HIGH);
-  Serial.println ("F");
-
-  delay (td);
-  digitalWrite (cathode_F, LOW);
-  digitalWrite (cathode_G, HIGH);
-  Serial.println ("G");
+  digitalWrite(anode_HH, LOW);
+  digitalWrite(anode_H, LOW);
+  digitalWrite(anode_MM, LOW);
+  digitalWrite(anode_M, LOW);
+  digitalWrite(cathode_A, HIGH);
+  Serial.println("A");
+  delay(td);
+  digitalWrite(cathode_A, LOW);
+  digitalWrite(cathode_B, HIGH);
+  Serial.println("B");
+  delay(td);
+  digitalWrite(cathode_B, LOW);
+  digitalWrite(cathode_C, HIGH);
+  Serial.println("C");
+  delay(td);
+  digitalWrite(cathode_C, LOW);
+  digitalWrite(cathode_D, HIGH);
+  Serial.println("D");
+  delay(td);
+  digitalWrite(cathode_D, LOW);
+  digitalWrite(cathode_E, HIGH);
+  Serial.println("E");
+  delay(td);
+  digitalWrite(cathode_E, LOW);
+  digitalWrite(cathode_F, HIGH);
+  Serial.println("F");
+  delay(td);
+  digitalWrite(cathode_F, LOW);
+  digitalWrite(cathode_G, HIGH);
+  Serial.println("G");
   delay(1000);
-  digitalWrite (cathode_G, LOW);
-  testcathodes();
+  digitalWrite(cathode_G, LOW);
 }
